fb0974bb3e94e88839e89d2ca8e9b4a9917d64b3
k <rmariana@gmail.com>
2020-11-29 11:32:40 +0100

    improve style

--- vm_cpu_perf.exs
+++ vm_cpu_perf.exs
@@ -2,45 +2,43 @@ defmodule VmCpuBench do
   @moduledoc """
   a measuring tool for the Erlang VM
 
-  Given an integer n  (number of iterations) as input, returns the time in `ms` to run the algorithm with n.
+  Given an integer `n` (number of iterations) as input, returns the time in `ms` to run the algorithm with n.
   The calculated time increases non linearly with n, try with low number like 300.
   I use it to compare vm on different hardwares and dimension loads.
   """
 
   def run(n) do
-    (fn n ->
-       self = self()
+    numbers = for _ <- 1..n, do: :rand.uniform(1000)
+    numbers_sz = length(numbers)
+    self = self()
 
-       g = fn
-         _, 0, _, l ->
-           l
+    recurse_fn = fn
+      _, 0, acc ->
+        acc
 
-         g1, n1, ref, l ->
-           receive do
-             {^ref, ret} -> g1.(g1, n1 - 1, ref, [ret | l])
-           end
-       end
+      g1, n1, acc ->
+        receive do: (val -> g1.(g1, n1 - 1, [val | acc]))
+    end
 
-       ref = make_ref()
+    worker_fn = fn ->
+      send(self, List.foldl(numbers, 1, fn _, _a -> Enum.sort(numbers) end))
+    end
 
-       l = for _ <- 1..n, do: :rand.uniform(1000)
-       fw = fn -> send(self, {ref, List.foldl(l, 1, fn _j, _a -> Enum.sort(l) end)}) end
+    bench_fn = fn ->
+      for _ <- numbers, do: spawn(worker_fn)
+      recurse_fn.(recurse_fn, numbers_sz, [])
+    end
 
-       {t, _v} =
-         :timer.tc(fn ->
-           for _i <- l, do: spawn(fw)
-           g.(g, length(l), ref, [])
-         end)
-
-       t / 1000
-     end).(n)
+    (fn ->
+       bench_fn
+       |> :timer.tc()
+       |> elem(0)
+       |> Kernel./(1000)
+     end).()
   end
 end
 
-iters =
-  IO.gets('')
-  |> String.trim_trailing()
-  |> String.to_integer()
+iters = IO.gets('') |> String.trim_trailing() |> String.to_integer()
 
 VmCpuBench.run(iters)
 |> IO.inspect(label: "computing time (ms.) to execute #{iters} iterations")
